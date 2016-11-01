bbd7fc49fe389eb947825679e0377a99a341d2ba
gchinellato <guilhermechinellato@gmail.com>
2016-11-01 01:15:39 -0200

    imu and motion module

---
+++ src/main.h
@@ -0,0 +1,31 @@
+/**
+*************************************************
+* @Project: Self Balance
+* @Platform: Arduino Nano ATmega328
+* @Description: Main thread
+* @Owner: Guilherme Chinellato
+* @Email: guilhermechinellato@gmail.com
+*************************************************
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#define SERIAL_BAUDRATE 	9600
+#define DATA_INTERVAL 	    20 // ms
+
+/* GPIO mapping */
+#define PWM1_PIN 9
+#define CW1_PIN 7
+#define CCW1_PIN 8
+
+#define PWM2_PIN 10
+#define CW2_PIN 5
+#define CCW2_PIN 6
+
+float dt=0; // duration time
+unsigned int timestamp;
+unsigned int timestamp_old;
+float *ori; // orientation vector (roll, pitch, yaw)
+
+#endif
